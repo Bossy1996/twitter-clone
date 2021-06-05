ad41d4ea09f820d9503a828e3b382df1c953391b
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-06-05 16:10:22 +0200

    stuff

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,16 @@
 from django.db import models
-
+import random
 # Create your models here.
 
 class Tweet(models.Model):
     id = models.AutoField(primary_key=True)
     content = models.TextField()
     media = models.FileField(upload_to='media/', blank=True, null=True)
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0, 100)
+        }
     
--- tweets/views.py
+++ tweets/views.py
@@ -23,6 +23,8 @@ def tweet_create_view(request, *args, **kargs):
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201)
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
@@ -34,7 +36,7 @@ def tweet_list_view(request, *args, **kwargs):
     return json data
     """
     qs = Tweet.objects.all()
-    tweet_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 10000000)} for x in qs] # the like is a place holder it has to change to a database field
+    tweet_list = [x.serialize() for x in qs] # the like is a place holder it has to change to a database field
     data = {
         "isUser": False,
         "response": tweet_list
