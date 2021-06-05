4dc9fb38b85fe03f93f7454e92acb3a824e03c71
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-06-05 15:45:35 +0200

    stuff

--- twitterclone/settings.py
+++ twitterclone/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0pqp-9rju1&(8tg)u4pofpnr5%@e78+^hdn3lz64f^qc%u=rix
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
