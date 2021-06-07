1d4c3bd59a2dcd3109df5d2b3b62ac89124943c2
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-06-07 22:25:46 +0200

    migration on models related to user

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.conf.global_settings import AUTH_USER_MODEL
+
 import random
 # Create your models here.
+User = AUTH_USER_MODEL
 
 class Tweet(models.Model):
     id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # many user can many tweets
     content = models.TextField()
     media = models.FileField(upload_to='media/', blank=True, null=True)
 
