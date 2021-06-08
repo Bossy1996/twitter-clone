d54661d5dfad29c9c68e51b92eb4a107ae580500
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-06-08 23:50:07 +0200

    added authentucation  to create tweet view

--- tweets/views.py
+++ tweets/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse, Http404, JsonResponse
 from django.utils.http import is_safe_url
 from django.conf.global_settings import ALLOWED_HOSTS #, LOGIN_URL
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from .forms import TweetForm
@@ -27,6 +28,7 @@ def home_view(request, *args, **kwargs):
     return redirect(LOGIN_URL)
 
 @api_view(['POST']) # http method the client === POST
+@permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
     serializer = TweetSerializer(data=request.POST)
     if serializer.is_valid(raise_exception=True):
