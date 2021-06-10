49c98545bdc4d08d2d491d1269e9de8393ec7190
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-06-10 17:03:51 +0200

    tweetlike and timestamp on liking and tweetcreate

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,4 @@
+from re import T
 from django.db import models
 from django.conf.global_settings import AUTH_USER_MODEL
 
@@ -5,11 +6,18 @@ import random
 # Create your models here.
 User = AUTH_USER_MODEL
 
+class  TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Tweet(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE) # many user can many tweets
+    likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
     content = models.TextField()
     media = models.FileField(upload_to='media/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-id']
