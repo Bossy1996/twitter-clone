9d47c9c5c0568beb4d25be04f273bdfb0bb03010
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-06-10 17:06:32 +0200

    tweetlike admin

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
+from django.db import models
 
-from .models import Tweet
+from .models import Tweet, TweetLike
+
+class TweetLikeAdmin(admin.TabularInline):
+    model = TweetLike
 
 class TweetAdmin(admin.ModelAdmin):
+    inlines = [TweetLikeAdmin]
     list_display = ["__str__", "user"]
-    search_fields = ["user__username", "user__email"]
+    search_fields = ["content" ,"user__username", "user__email"]
     
     class Meta:
         model = Tweet
