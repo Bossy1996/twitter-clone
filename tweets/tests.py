c36d00cd0816fe0b2b156c09e700b3aa932da04c
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-06-12 15:52:53 +0200

    Test

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+from .models import Tweet
 # Create your tests here.
+User = get_user_model()
+
+class TweetTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='abc', password='somepassword')
