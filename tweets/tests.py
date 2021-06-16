ae2533ff22be9f6c73933de173d22bcc1ddf010a
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-06-16 17:06:00 +0200

    tweetlist, tweet create test

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import TestCase, client
+
+from rest_framework.test import APIClient
 
 from .models import Tweet
 # Create your tests here.
@@ -7,10 +9,21 @@ User = get_user_model()
 
 class TweetTestCase(TestCase):
     def setUp(self):
-        User.objects.create_user(username='abc', password='somepassword')
+        self.user = User.objects.create_user(username='def', password='somepassword')
+        Tweet.objects.create(content="My first tweet", user=self.user)
 
-# this is a cheat comment because it's too hot outside, and I'm broken mentally i can't even get out of my bed.
-# sorry for this but i trully can't.
-# i don't know if i'm coding today but probably yes. But becuase i don't know i make this commit
-# It's 17:35 and it's too hot outside i've been taking a nap almost ultil an hour ago so i don't know what i'm gonna do today 
-# I hope i can code at least a couple hour once it gets colder because i trully want to die
+    def test_tweet_created(self):
+        tweet = Tweet.objects.create(content="My second tweet", user=self.user)
+        self.assertEqual(tweet.id, 2)
+        self.assertEqual(tweet.user, self.user)
+
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password='somepassword')
+        return client
+    
+    def test_tweet_list(self):
+        client = self.get_client()
+        response = client.get("/api/tweets/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()))
