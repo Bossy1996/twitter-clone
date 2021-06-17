698dff30029e0c39fc4e1f61f75882d024ab162a
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-06-17 20:54:30 +0200

    Lookup

--- twitterclone-web/src/tweets/components.js
+++ twitterclone-web/src/tweets/components.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import {useEffect, useState} from 'react';
 
-const loadTweets = (callback) => {
-    const xhr = new XMLHttpRequest()
-    const mehotd = 'GET'
-    const url = "http://localhost:8000/api/tweets"
-    const responseType = "json"
-    xhr.responseType = responseType
-    xhr.open(mehotd, url)
-    xhr.onload = () => {
-      callback(xhr.response, xhr.status)
-    }
-    xhr.onerror = function (error) {
-      console.log(error)
-      callback({"message": "The request was an error"}, 400)
-    }
-    xhr.send()
-  }
+import { loadTweets } from '../lookup';
   
 export function TweetsList(props) {
     const [tweets, setTweets] = useState([]);
