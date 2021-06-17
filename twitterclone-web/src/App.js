a04011d5195035458a15a89ddeba38d9f56fceda
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-06-17 20:36:28 +0200

    Action buttom

--- twitterclone-web/src/App.js
+++ twitterclone-web/src/App.js
@@ -18,12 +18,22 @@ const loadTweets = (callback) => {
   }
   xhr.send()
 }
-  
+
+function ActionBtn(props){
+  const {tweet, action} = props
+  const className = props.className ? props.className: 'btn btn-small btn-primary'
+  return action.type === 'like' ? <buttom className={className} >{tweet.likes} Like</buttom>: null
+}
+
 function Tweet(props) {
   const {tweet} = props
   const className = props.className ? props.className: 'col-10 mx-auto col-md-6'
   return <div className={className}>
     <p> {tweet.id} - {tweet.content}</p>
+    <div className='btn btn-primary'>
+      <ActionBtn tweet={tweet} action={{type: 'like'}}/>
+      <ActionBtn tweet={tweet} action={{type: 'unlike'}}/>
+    </div>
   </div>
 }
 
