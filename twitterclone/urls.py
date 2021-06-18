9aa89423e5ceb29567199f293f4de87bf90c7ff0
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-06-18 04:06:39 +0200

    static root

--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from tweets.views import (
     home_view, 
@@ -33,3 +35,6 @@ urlpatterns = [
     path('create-tweet', tweet_create_view),
     path('api/tweets/', include('tweets.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
