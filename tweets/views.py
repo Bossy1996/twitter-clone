effebdd07f289c632f5535d72d507b10e237b75a
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-07-22 14:55:26 +0200

    views placeholder

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def create_tweet_view():
+    """
+    Creates a new Tweet
+    """
+    pass
+
+def delete_tweet_view():
+    """
+    Deletes a Tweet based on  tweet_id
+    """
+    pass
+
+def action_tweet_view():
+    """
+    Three actions: Like, Unlike and Retweet
+    """
+    pass
+
+def tweet_detail_view():
+    """In Detail view of the tweet, requieres tweet_id"""
+    pass
+
+def tweet_list_view():
+    """Tweet list view where it's show all the tweets"""
+    pass
