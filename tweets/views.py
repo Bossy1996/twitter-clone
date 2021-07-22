910fbb689b7da098b2c8b98d3b50cf5b7d19a446
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-07-22 15:43:18 +0200

    Create Tweet view

--- tweets/views.py
+++ tweets/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import action, api_view, permission_classes, authentication_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .models import Tweet
+from .serializers import (
+    TweetSerializer,
+    TweetCreateSerializer,
+    TweetActionSerializer
+)
+
 # Create your views here.
 
-def create_tweet_view():
+def create_tweet_view(request, *args, **kwargs):
     """
     Creates a new Tweet
     """
+    serializer = TweetSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=201)
+    return Response({}, status=400)
     pass
 
 def delete_tweet_view():
