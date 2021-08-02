f9fe18ccc9e2cee28e95af7043f26d9314db118e
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-08-02 22:46:16 +0200

    User Admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
+from users.managers import CustomUserManager
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+from .serializers import UserCreationSerializer, UserChangeSerializer
+
+class CustomUserAdmin(UserAdmin):
+
+    list_display = ('email', 'is_staff', 'is_active')
+    list_filter = ('email', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ['email']
+    ordering = ('email')
+
+    class Meta:
+        Model = User
+
+
+admin.site.register(User, CustomUserManager)
