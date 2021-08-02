dd300eabe8c7d93f883bc5bc8812569b782d7ba3
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-08-02 22:46:25 +0200

    User custom Model

--- users/models.py
+++ users/models.py
@@ -4,13 +4,16 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from .managers import CustomUserManager
+from tweets.models import Tweet
 
 class User(AbstractBaseUser, PermissionsMixin):
     
     email = models.EmailField(_('email address'), unique=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
+    # TODO: Every user has its own tweets, liked tweet and retweeted
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
