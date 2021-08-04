b12d979e66fba199946ac131f1a4c491397e853e
Jon Andoni Galdos <jonandonigaldos1996@gmail.com>
2021-08-04 18:12:31 +0200

    db migrtions

--- twitterclone/urls.py
+++ twitterclone/urls.py
@@ -20,21 +20,21 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 from tweets.views import (
-    home_view, 
+    # home_view, 
     tweet_list_view, 
     tweet_detail_view, 
-    tweet_create_view, 
-    tweet_delete_view, 
-    tweet_action_view
+    create_tweet_view, 
+    # tweet_delete_view, 
+    # tweet_action_view
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view),
+    # path('', home_view),
     path('react/', TemplateView.as_view(template_name='react_via_dj.html')),
     path('tweet/', tweet_list_view),
     path('tweet/<int:tweet_id>', tweet_detail_view),
-    path('create-tweet', tweet_create_view),
+    path('create-tweet', create_tweet_view),
     path('api/tweets/', include('tweets.urls'))
 ]
 
